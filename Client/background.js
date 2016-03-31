035ba5145b2372aa501363825ac4ba67aaa273f8
Karan Ganju <karanganju9@gmail.com>
2016-04-01 00:06:52 +0530

    Small Changes

--- Client/background.js
+++ Client/background.js
@@ -1,55 +1,3 @@
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
-
-function sendPost(url, postData)
-{
-  var method = "POST";
-  var async = true;
-  var request = new XMLHttpRequest();
-
-  request.onload = function () {
-     var status = request.status;
-     var data = request.responseText;
-  }
-
-  request.open(method, url, async);
-  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  request.send(postData);
-}
-
-function send_Init()
-{
-
-}
-
-function send_ChallengeResponse()
-{
-  
-}
-
-
-function init_ZKP(url)
-{
-  send_Init();
-  send_ChallengeResponse(); 
-}
-
 function find_name_value_in_arr(nameStr, valueStr, arr){
   for(i = 0; i < arr.length ; i++) if(arr[i].name == nameStr && arr[i].value == valueStr) return true;
   return false;
@@ -71,4 +19,5 @@ chrome.webRequest.onHeadersReceived.addListener(
     ],
   },
   // extraInfoSpec
-  ["responseHeaders"]);
+  ["responseHeaders"]
+);
--- Client/popupjs.js
+++ Client/popupjs.js
@@ -1,18 +1,64 @@
-// function check(form) { /*function to check userid & password*/
-//     /*the following code checkes whether the entered userid and password are matching*/
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
 
-      window.onload = function() {
-        var myButton = document.getElementById("btn-login");
-        var form = document.getElementById("alpha");
-        init_ZKP(form.userid.value,form.pswrd.value);
-        myButton.addEventListener('click', function(){alert(form.pswrd.value);}, false);
-        //document.getElementById("btn-login").onClick = function()console.log(form.pswrd.value);
-      }
+
+function sendPost(url, postData)
+{
+  var method = "POST";
+  var async = true;
+  var request = new XMLHttpRequest();
+
+  request.onload = function () {
+     var status = request.status;
+     var data = request.responseText;
+  }
+
+  request.open(method, url, async);
+  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  request.send(postData);
+}
+
+function send_Init()
+{
+
+}
+
+function send_ChallengeResponse()
+{
+  
+}
+
+
+function init_ZKP(url)
+{
+  send_Init();
+  send_ChallengeResponse(); 
+}
 
 function init_ZKP(username, password){
-	var shaObj = new jsSHA("SHA-512", "TEXT");
-	shaObj.update(password);
-	var hash = shaObj.getHash("HEX");
-	
+  var shaObj = new jsSHA("SHA-512", "TEXT");
+  shaObj.update(password);
+  var hash = shaObj.getHash("HEX");
+  alert(hash);
 }
-// }
+
+window.onload = function() {
+  var myButton = document.getElementById("btn-login");
+  var form = document.getElementById("form-login");
+  myButton.addEventListener('click', function(){init_ZKP(form.userid.value,form.pswrd.value);}, false);
+}
