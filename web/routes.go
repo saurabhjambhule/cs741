334d5de1a4cd9ba9c6daa64261c63962712b19bf
Saurabh Jambhule <saurabhjambhule@gmail.com>
2016-03-31 15:54:43 +0530

    point generated

--- web/routes.go
+++ web/routes.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"crypto/elliptic"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"text/template"
 )
 
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello, world\n")
+	tmpCurve := elliptic.P256()
+	fmt.Println(tmpCurve.Params().Gx)
+	fmt.Println(tmpCurve.Params().Gy)
+
+	randNo := rand.Uint32()
+	fmt.Println(randNo)
+
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, randNo)
+	fmt.Println(bs)
+
+	newX, newY := tmpCurve.ScalarBaseMult(bs)
+	fmt.Println(newX, newY)
+
+	fmt.Println(tmpCurve.IsOnCurve(newX, newY))
+	strX := newX.String()
+	strY := newY.String()
+	Point := strX + " " + strY
+
+	fmt.Println(Point)
+	w.Header().Set("Access-Control-Allow-Origin", "Point")
+	//r.Header.Add("Point", Point)
+
+	//io.WriteString(w, Point)
+
 }
 
 func HandlePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Page Loaded")
+	w.Header().Set("Server", "ZKP")
+	//w.WriteHeader(201)
 	title := r.URL.Path[len("/"):]
 	p, err := loadPage(title)
 	if err != nil {
--- web/tests.sh
+++ web/tests.sh
@@ -1,4 +1,4 @@
 curl http://0.0.0.0:9090
-curl http://0.0.0.0:9090/index.html
+curl -i http://0.0.0.0:9090/index.html
 curl -u username:password -d "param1=value1&param2=value2" http://0.0.0.0:9090/post  
 curl -u username:password http://0.0.0.0:9090/json 
