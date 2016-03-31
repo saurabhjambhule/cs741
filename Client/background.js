c53d382e406b2271c3cb62c1f85fc44518fabf34
Karan Ganju <karanganju9@gmail.com>
2016-03-31 16:11:04 +0530

    Changed a few things...

--- Client/background.js
+++ Client/background.js
@@ -20,44 +20,34 @@ function readTextFile(file)
 function sendPost(url, postData)
 {
   var method = "POST";
-
-  // You REALLY want async = true.
-  // Otherwise, it'll block ALL execution waiting for server response.
   var async = true;
-
   var request = new XMLHttpRequest();
 
-  // Before we send anything, we first have to say what we will do when the
-  // server responds. This seems backwards (say how we'll respond before we send
-  // the request? huh?), but that's how Javascript works.
-  // This function attached to the XMLHttpRequest "onload" property specifies how
-  // the HTTP response will be handled. 
   request.onload = function () {
-
-     // Because of javascript's fabulous closure concept, the XMLHttpRequest "request"
-     // object declared above is available in this function even though this function
-     // executes long after the request is sent and long after this function is
-     // instantiated. This fact is CRUCIAL to the workings of XHR in ordinary
-     // applications.
-
-     // You can get all kinds of information about the HTTP response.
-     var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
-     var data = request.responseText; // Returned data, e.g., an HTML document.
+     var status = request.status;
+     var data = request.responseText;
   }
 
   request.open(method, url, async);
-
   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  // Or... request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-  // Or... whatever
-
-  // Actually sends the request to the server.
   request.send(postData);
 }
 
-function init_ZKP(url)
+function send_Init()
+{
+
+}
+
+function send_ChallengeResponse()
 {
   
+}
+
+
+function init_ZKP(url)
+{
+  send_Init();
+  send_ChallengeResponse(); 
 }
 
 chrome.webRequest.onHeadersReceived.addListener(
@@ -65,13 +55,6 @@ chrome.webRequest.onHeadersReceived.addListener(
     if(info.statusCode == 104) {
       init_ZKP(info.url);
     }
-    // if (info.url.indexOf("www.") == -1) return {cancel: false};
-    // var p = info.url.substr(info.url.indexOf("www.")+1).split("&");
-    // for (var item in p) {
-    //   var key = p[item].substr(p[item].indexOf("=")+1);
-    //   if (checkxss(decodeURIComponent(JSON.stringify(key)).replace(/\+/gm," "))) return {cancel: true};
-    // }
-    // return {cancel: false};
   },
   // filters
   {
