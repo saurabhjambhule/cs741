37bf734779c2467597843910bd39c512a9a10d46
dheeraj <kdheeraj784@gmail.com>
2016-04-05 01:02:44 +0530

    Login and Reister should work -> Have to debug

--- Server/Js/ZKP.js
+++ Server/Js/ZKP.js
@@ -1,4 +1,5 @@
 function fromHex(s) { return new BigInteger(s, 16); }
+function fromDec(s) { return new BigInteger(s, 10); }
 
 var p = fromHex("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF");
 var a = fromHex("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC");
@@ -7,69 +8,39 @@ var n = fromHex("FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC63255
 var h = BigInteger.ONE;
 var curve = new ECCurveFp(p, a, b);
 
-function fetchJSONFile(url, postData, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
-        }
-    };
-    httpRequest.open('POST', url, false);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    httpRequest.send(postData);
-}
-
-function get_item_from_storage(item) {
-    if (localStorage.getItem(item)) return localStorage.getItem(item);
-    else {
-        alert("ZKP failed!");
-        return false;
-    }
-}
-
 function ECC_save_init(data) {
     for (var key in data) {
-      localStorage.setItem(key.toString(),data[key]);
+      localStorage.setItem(key.toString(),fromDec(data[key]).toString(16));
     }
 }
 
 function ECC_get_init(gen) {
-  alert("in");
-   var G = curve.decodePointHex("04"
-                + "6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296"
-                + "4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5");
-    var x = fromHex("6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296");
-    var y = fromHex("4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5");
-    alert(x+"\n"+y+"\n"+n+"\n"+h+"\n"+a+"\n"+b+"\n"+p);
-   //var genPt = new ECPointFP(curve, x, y, null);
-//  alert(curve.q);
-    // var genPt = new ECPointFP(curve, x, y, null);
-    //var t= pointFpGetX();
-
-
+    var G = curve.decodePointHex("04" + fromDec(gen["x"]).toString(16) + fromDec(gen["y"]).toString(16));
     var randNo = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-    var hexString = randNo.toString(16);
-    var randNo=fromHex(hexString);
-    var ptA = G.multiply(randNo);
-    alert(ptA);
-      return "Initial stuff";
-    //send to Server ptA and receive c
-
-    //Compute c+r.x and send to server
+    var hexString = fromHex(randNo.toString(16));
+    localStorage.setItem("rand",hexString);
+    var randpt = G.multiply(hexString);
+    return randpt.getX().toBigInteger().toString(10) + "," + randpt.getY().toBigInteger().toString(10);
 }
 
-function ECC_gen_response(challenge) {
-    return "Arbit FART!";
+function ECC_gen_response(challenge, hash) {
+    var r = fromHex(localStorage.getItem("rand"));
+    var c = fromDec(challenge);
+    var x = fromHex(hash);
+    var multres;
+    x.multiplyTo(c,multres);
+    var addres;
+    multres.SubTo(r.negate(),addres);
+    var result = addres.mod(p);
+    return result.toString(10);
 }
 
 function ECC_create_reg_value(hash) {
+    document.getElementById("gen").value = localStorage.getItem("x").toString() + localStorage.getItem("y").toString();
     var hash_bigInt = fromHex(hash.toString());
     var G = curve.decodePointHex("04" + localStorage.getItem("x").toString() + localStorage.getItem("y").toString());
     var pub_key = G.multiply(hash_bigInt);
-    return pub_key.getX().toBigInteger().toString(16) + "," + pub_key.getY().toBigInteger().toString(16);
+    return pub_key.getX().toBigInteger().toString(10) + "," + pub_key.getY().toBigInteger().toString(10);
 }
 
 function get_ZKP_inits(gen) {
@@ -80,8 +51,8 @@ function save_ZKP_inits(data) {
     ECC_save_init(data);
 }
 
-function generate_ZKP_response(challenge) {
-    return ECC_gen_response(challenge);
+function generate_ZKP_response(challenge, hash) {
+    return ECC_gen_response(challenge, hash);
 }
 
 function create_ZKP_reg_value(hash) {
@@ -96,26 +67,25 @@ function login_ZKP() {
     var shaObj = new jsSHA("SHA-256", "TEXT");
     shaObj.update(password);
     var hash = shaObj.getHash("HEX");
-    // alert("in");
 
-    // var req = new XMLHttpRequest();
-    // req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // req.open('POST', "login-basic", false);
-    // req.send("state=login&step=0");
-    // var reply = JSON.parse(req.responseText);
-    //
-     var data = get_ZKP_inits("temo");
-    // var req = new XMLHttpRequest();
-    // req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // req.open('POST', "login-basic", false);
-    // req.send("state=login&step=1&login_init_data=" + data.toString());
-    // var challenge = JSON.parse(req.responseText);
-    //
-    // var response = generate_ZKP_response(challenge);
-    // req = new XMLHttpRequest();
-    // req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // req.open('POST', "login-basic", false);
-    // req.send("state=login&step=1&login_response_data=" + response.toString());
+    var req = new XMLHttpRequest();
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.open('POST', "login-basic", false);
+    req.send("state=login&step=0");
+    var reply = JSON.parse(req.responseText);
+
+    var data = get_ZKP_inits(reply);
+    var req = new XMLHttpRequest();
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.open('POST', "login-basic", false);
+    req.send("state=login&step=1&login_init_data=" + data.toString());
+    var challenge = JSON.parse(req.responseText);
+
+    var response = generate_ZKP_response(challenge, hash);
+    req = new XMLHttpRequest();
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.open('POST', "login-basic", false);
+    req.send("state=login&step=2&login_response_data=" + response.toString());
 }
 
 function register_ZKP(form) {
@@ -127,6 +97,13 @@ function register_ZKP(form) {
         return;
     }
 
+    var shaObj = new jsSHA("SHA-256", "TEXT");
+    shaObj.update(form.psswd.value);
+    var uname = form.userid.value;
+    var hash = shaObj.getHash("HEX");
+
+    document.getElementById("pwdhash").value = hash;
+
     var req = new XMLHttpRequest();
     req.open('POST', "register-basic", false);
     req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -136,16 +113,10 @@ function register_ZKP(form) {
     //Save data
     save_ZKP_inits(data);
 
-    var shaObj = new jsSHA("SHA-256", "TEXT");
-    shaObj.update(form.psswd.value);
-    var uname = form.userid.value;
-    var hash = shaObj.getHash("HEX");
-
     //Send data
     var reg_value = create_ZKP_reg_value(hash);
-    alert(reg_value);
+    document.getElementById("pubkey").value = reg_value;
     var req1 = new XMLHttpRequest();
-
     req1.open('POST', "register-basic", false);
     req1.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     req1.send("state=register&step=1&uname="+ uname +"&pub_key=" + reg_value.toString());
